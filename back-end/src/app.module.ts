6169b38d581aad9d2e7f81b299f76674ca81059b
Blu-ea <antoine.miguez@gmail.com>
2023-07-01 00:26:18 +0200

    Connection done with db Next neet to get ant put data on it

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,21 +3,24 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
 import * as process from 'process';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT),
-      username: process.env.PROTGRES_USER,
-      password: process.env.PROTGRES_PASSWORD,
-      database: process.env.PROTGRES_DB,
-      autoLoadEntities: true,
-      synchronize: true,
+      port: parseInt(process.env.POSTGRES_PORT) || 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: false, // true -> will create the Table on db if class not there
     }),
     UsersModule,
   ],
--- back-end/src/users/models/users.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface UsersInterface {
-  id?: number;
-  username?: string;
-  status?: number;
-  avatar_path?: string;
-  token_2fa?: string;
-  has_2fa: boolean;
-}
