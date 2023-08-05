207728ae4507d38981882355b1482fcb1fa869dc
benJ3D <benjamin@ben3d.fr>
2023-08-05 19:31:14 +0200

    page chatroom- passage en full largeur

--- front-end/src/app/(ben proto)/proto/chat/page.tsx
+++ front-end/src/app/(ben proto)/proto/chat/page.tsx
@@ -4,7 +4,7 @@ export default function ChatRoom() {
 	return (
     <div>
     <div className=' text-white text-center font-semibold text-3xl mt-4'>CHAT ROOM PROTO</div>
-    <ChatRoomCommponent className='h-[90vh] max-w-max pb-5'/>
+    <ChatRoomCommponent className='h-[90vh] w-screen pb-5'/>
   </div>
 	)
 }
