d196082befac3927cf7820ed8e01516bdc38cff7
bducrocq <benjamin@ben3d.fr>
2023-09-07 15:59:06 +0200

    update link

--- back-end/src/module.game/game/ServerGame.ts
+++ back-end/src/module.game/game/ServerGame.ts
@@ -1,7 +1,7 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Matchmaking } from './Matchmaking';
 import { GameSession } from './GameSession';
-import { WebsocketGatewayGame } from '../../websocket/wsGame.gateway';
+import { WebsocketGatewayGame } from '../websocket/wsGame.gateway';
 import { Server, Socket } from 'socket.io';
 import { userInfoSocket, Stack, EGameMod } from 'shared/typesGame';
 import { v4 as uuidv4 } from "uuid";
@@ -31,7 +31,6 @@ export class ServerGame {
     }
   }
 
-  
   public removePlayerToMatchmaking(player: userInfoSocket) {
     if (!player)
     return console.log('removePlayerToMatchmaking: Error player')
