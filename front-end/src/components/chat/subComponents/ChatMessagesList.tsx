8ca60ff93d251ebdedaf6c4e6a9c1f3eda144e28
DUCROCQ Benjamin <benjamin@ben3d.fr>
2023-09-28 04:23:57 +0200

    add end button for scroll to last message

--- front-end/src/components/chat/subComponents/ChatMessagesList.tsx
+++ front-end/src/components/chat/subComponents/ChatMessagesList.tsx
@@ -26,11 +26,11 @@ export default function ChatMessagesList({className, messages}: {className: stri
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = refDivParent.current?.scrollTop;
-      console.log("Position actuelle du scroll :", scrollPosition);
+      // console.log("Position actuelle du scroll :", scrollPosition);
       if (refDivParent && scrollPosition)
       {
         const maxHeight = refDivParent.current?.scrollHeight - refDivParent.current?.clientHeight;
-        if (scrollPosition < (maxHeight - 20))
+        if (scrollPosition < (maxHeight - 400))
           setButtonGoToEnd(true)
         else
           setButtonGoToEnd(false)
@@ -51,37 +51,43 @@ export default function ChatMessagesList({className, messages}: {className: stri
       console.log("Position maximale du scroll :", maxHeight);}
   }, [refDivParent.current]);
   
+  
+  const btnGoToEnd = (classNameOverride?) => {
+    return (
+      <button className={`text-sm text-orange-900 pointer-events-auto z-10 ${classNameOverride}`} onClick={goToEndMessage}>GOTO END</button>
+    )
+  }
 
   return (
-    <div className={`${className}`} ref={refDivParent} >
-      {buttonGoToEnd &&
-        <button className=' text-sm text-orange-900' onClick={goToEndMessage}>GOTO END</button>}
-      {messages.map((obj, index) => (
-        <div key={'div' + index}>
-          {obj.ownerUser.UserID > 0 ?
-            <div key={'blocMessage_' + index} className='chat_message_list_block'>
-              <li key={'nickname_' + index}
-                className={`chat_message_list_nickname ${obj.ownerUser.UserID === 2 ?
-                  'chat_message_list_nickname_right' : 'chat_message_list_nickname_left'}`}>
-                {obj.ownerUser.nickname} ({obj.ownerUser.login})
-              </li>
-              <li key={'messageContent_' + index}
-                className={`chat_message_list_content ${obj.ownerUser.UserID === 2 ?
-                  'chat_message_list_content_right' : 'chat_message_list_content_left'}`}>
-                {obj.content}
-              </li>
+    <div className={`${className} `} >
+      <div className={`chat_message_list_sub`} ref={refDivParent} >
+        {/* {buttonGoToEnd && btnGoToEnd()} */}
+        {messages.map((obj, index) => (
+          <div key={'div' + index}>
+            {obj.ownerUser.UserID > 0 ?
+              <div key={'blocMessage_' + index} className='chat_message_list_block'>
+                <li key={'nickname_' + index}
+                  className={`chat_message_list_nickname ${obj.ownerUser.UserID === 2 ?
+                    'chat_message_list_nickname_right' : 'chat_message_list_nickname_left'}`}>
+                  {obj.ownerUser.nickname} ({obj.ownerUser.login})
+                </li>
+                <li key={'messageContent_' + index}
+                  className={`chat_message_list_content ${obj.ownerUser.UserID === 2 ?
+                    'chat_message_list_content_right' : 'chat_message_list_content_left'}`}>
+                  {obj.content}
+                </li>
 
-            </div>
-            :
-            <div key={'messageSystem-' + index} className='chat_message_list_content_system'>
-              {obj.content}
-            </div>}
-        </div>
-      ))
-      }
-      <div ref={refDivEndMessage} />
-      {buttonGoToEnd &&
-        <button className='' onClick={goToEndMessage}>END</button>}
+              </div>
+              :
+              <div key={'messageSystem-' + index} className='chat_message_list_content_system'>
+                {obj.content}
+              </div>}
+          </div>
+        ))
+        }
+        <div ref={refDivEndMessage} />
+      </div>
+      {buttonGoToEnd && btnGoToEnd(`text-xs mt-2`)}
     </div>
   )
 }
