2f09c319a4b8b603376199a8eb5fd1cef980a796
Blue <82770859+Blu-ea@users.noreply.github.com>
2023-10-25 17:00:02 +0200

    Delete back-end/src/shared directory
    No more Shared file in the backend my default

--- back-end/src/shared/entities/IBanned.entity.ts
+++
@@ -1,9 +0,0 @@
-import {IUserEntity} from './IUser.entity';
-import {IChannelEntity} from './IChannel.entity';
-
-export interface IBannedEntity {
-	bannedID: number;
-	user: IUserEntity;
-	channel: IChannelEntity;
-	endTime: Date;
-}
--- back-end/src/shared/entities/IInvite.entity.ts
+++
@@ -1,9 +0,0 @@
-import {IUserEntity} from './IUser.entity';
-import {IChannelEntity} from './IChannel.entity';
-
-export interface IInviteEntity {
-	inviteID: number;
-	user: IUserEntity;
-	channel: IChannelEntity;
-	sender: IUserEntity;
-}
